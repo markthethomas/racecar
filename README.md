e73cc77a683f7b62301463c61b37ffdbbd7a97f5
Mark Thomas <markthethomas@gmail.com>
2014-11-29 11:32:20 -0800

    inital commit:

---
+++ README.md
@@ -0,0 +1,6 @@
+racecar
+===========
+
+Checks if a given number n is palindromic and returns the lowest base for which n is a palindrome.
+
+Given n, it returns the lowest base at which n is a palindrome. Fun, no?
--- palindomic.py
+++ palindomic.py
@@ -1,28 +1,3 @@
-# The sign outside reads: Name no one man.
-
-# "Escape. We must escape." Staring at the locked door of his cage, Beta Rabbit, spy and brilliant mathematician, has a
-# revelation. "Of course! Name no one man - it's a palindrome! Palindromes are the key to opening this lock!"
-
-# To help Beta Rabbit crack the lock, write a function answer(n) which returns the smallest positive integer base b, at least 2, in
-# which the integer n is a palindrome. The input n will satisfy "0 <= n <= 1000."
-
-
-# Test cases
-# ==========
-
-# smallest positive integer base b, at least 2, integer n is palindrome
-
-# Inputs:
-#     (int) n = 0
-# Output:
-#     (int) 2
-#
-# Inputs:
-#     (int) n = 42
-# Output:
-#     (int) 4
-
-
 import string
 digs = string.digits + string.lowercase
 
